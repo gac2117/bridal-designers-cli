295d1dbab0942dcb08c7093526a9f836d7fca2fc
Grace Choi <oiograceoio@gmail.com>
2019-03-09 17:14:15 +0000

    modified files

--- lib/bridal_designers.rb
+++ lib/bridal_designers.rb
@@ -1,3 +1,8 @@
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+
 require_relative "./bridal_designers/version"
 require_relative "./bridal_designers/cli"
 require_relative "./bridal_designers/designer"
