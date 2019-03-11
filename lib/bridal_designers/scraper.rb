6b34e57313d3e3c40dc202dab6db165b67efab29
Grace Choi <oiograceoio@gmail.com>
2019-03-11 18:37:27 +0000

    modified scraper

--- lib/bridal_designers/scraper.rb
+++ lib/bridal_designers/scraper.rb
@@ -3,7 +3,7 @@ class BridalDesigners::Scraper
   def self.scrape_designers
     page = Nokogiri::HTML(open("https://www.gildedbridal.com/bridal-designers"))
 
-    scraped_designers = page.css(".q_elements_item")
+    scraped_designers = page.css(".qode-advanced-tab-container").first.values("tab-wedding-gowns-260")
     binding.pry
     scraped_designers.each do |d|
       BridalDesigners::Designer.create_from_page(d)
