f8a73cd1b81ba47d4a14ff3b6bf67c8f8b6b9b02
Grace Choi <oiograceoio@gmail.com>
2019-03-12 18:54:35 +0000

    modified scraper

--- lib/bridal_designers/designer.rb
+++ lib/bridal_designers/designer.rb
@@ -8,21 +8,21 @@ class BridalDesigners::Designer
     designers = Array.new
     page = Nokogiri::HTML(open("https://www.gildedbridal.com/bridal-designers"))
     gowns = page.css("div.qode-advanced-tab-container").first
-    scraped_designers = gowns.css("div.wpb_text_column div")
+    scraped_designers = gowns.css("div.wpb_text_column")
     scraped_designers.each do |d|
       designer = {}
       designer[:name] = d.css("h2").text
-      if d.css('p') != nil
+      if d.search('p').empty?
+        designer[:location] = d.css(".wpb_wrapper").first.css('div')[0].text
+        designer[:description] = d.css(".wpb_wrapper").first.css('div')[2].text
+      else
         designer[:location] = d.css("p")[0].text
         designer[:description] = d.css('p')[1].text
-      else
-        designer[:location] = d.css("div")[0].text
-        designer[:description] = d.css('div')[1].text
       end
       designer[:url] = d.css('h2.gallery-title a').map { |e| e.attribute('href').value}
       designers << designer
-      binding.pry
     end
+    binding.pry
     designers
   end
 
